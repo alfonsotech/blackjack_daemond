93bc4908c2a66b5256e4092c0acc68dd615a29bd
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-11-29 12:49:12 -0800

    added TODOs to app.js file

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//TODO: attach card images to generatd values
+//Could also just build an array of objects and randomly select an object to pull from
+//TODO: If an "A" is dealt, give player option to use the Ace as a 1 0r 11 value.
+
+
+//TODO: Function to add the values of cards, to be used to generate values for inital player cards, new value after hit, and dealer cardsonce both are revealed.
+
+//EVENT LISTENERS
+//TODO: on click of new hand button, randomly generate values for 2 player cards and 1 dealer upcard.
+//TODO: on click of hit button, generate a new card/value and add it to the total score
+//TODO: on click of stand button, reveal(generate) dealer second card. Series of messages telling user where the dealer is at and what action dealer needs to take
+
+//END GAME
+//TODO: function to alert player or dealer bust
+//TODO: Who won message, instructions for how to start a new hand. Should the message be different depending on whether player or dealer won?
