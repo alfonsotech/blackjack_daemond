a342802ce93fdef204e818aa38edf8f0ab4197ea
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-02 14:37:34 -0800

    added spaces in-between parens, brackets, etc; deleted spaces where not necessary; deleted some deprecated css rules for removed elements

--- scripts/models/cardsModel.js
+++ scripts/models/cardsModel.js
@@ -3,41 +3,38 @@
 DeckOfCards.all = [];
 
 //Object constructor
-function DeckOfCards(opts){
-  for(var keys in opts){
-    this[keys] = opts[keys];
+function DeckOfCards( opts ){
+  for( var keys in opts ){
+    this[ keys ] = opts[ keys ];
   }
 }
 
-//Q: when do i put methods on prototype versus on constructed object?
-
-//Method to load a deck of cards from data
-DeckOfCards.loadAll = function(data) {
-  data.forEach(function(element) {
-    DeckOfCards.all.push(new DeckOfCards(element));
+//Method:loadAll
+//loads a deck of cards from data
+DeckOfCards.loadAll = function( data ) {
+  data.forEach( function( element ) {
+    DeckOfCards.all.push(new DeckOfCards( element ));
   });
   // console.log('loaded deck of cards:', DeckOfCards.all);
 };
 
-//Method to get json cards data and pass it to the load function to make a fresh deck of cards
+//Method: fetchAll
+//gets json cards data & pass it to the load function, makes a fresh deck of cards
 DeckOfCards.fetchAll = function() {
-  $.ajax('data/cards.json', {
+  $.ajax( 'data/cards.json', {
     method: 'GET',
     success: successHandler,
     error: errorHandler
   });
-  function successHandler(data) {
-    DeckOfCards.loadAll(data);
-
-    // cardsView.toHtml(); TODO:add this once func is written
+  function successHandler( data ) {
+    DeckOfCards.loadAll( data );
   };
-  function errorHandler(error){
-    console.log('Error', error);
+  function errorHandler( error ){
+    console.log( 'Error', error );
   };
 };
-
 
 //Method to display cards to HTML
 // DeckOfCards.prototype.toHtml = function(){
 //
-// } //ES6 confused about where i use semi-colons and about if i can use var with constructors
+// } //ES6 confused about where i use semi-colons
--- scripts/views/cardsView.js
+++ scripts/views/cardsView.js
@@ -1,5 +1,5 @@
 'use strict'
-$( document ).ready(function() {
+$( document ).ready( function() {
 DeckOfCards.fetchAll();
 
 let cardsView = {};
@@ -43,7 +43,6 @@ cardsView.render = function( cards ) {
       + '" alt="' + card.alt + '"></li>' );
     }
   }
-
 }
 
 //Deal a new hand from card deck in use
@@ -53,17 +52,17 @@ cardsView.newHand = function() {
   playerCards = [];
   dealerCards = [];
   //draw three cards
-  $('#hit').removeAttr('disabled');
-  $('#stand').removeAttr('disabled');
-  $('#surrender').removeAttr('disabled');
+  $( '#hit' ).removeAttr( 'disabled' );
+  $( '#stand' ).removeAttr( 'disabled' );
+  $( '#surrender' ).removeAttr( 'disabled' );
   let playerCard1 = cardsView.hit();
-  playerCards.push(playerCard1);
+  playerCards.push( playerCard1 );
   let playerCard2 = cardsView.hit();
-  playerCards.push(playerCard2);
+  playerCards.push( playerCard2 );
   let dealerCard1 = cardsView.hit();
-  dealerCards.push(dealerCard1);
-  console.log(dealerCards);
-  console.log(playerCards);
+  dealerCards.push( dealerCard1 );
+  console.log( dealerCards );
+  console.log( playerCards );
   // drawPlayerCards(playerCard1, playerCard2);
   // drawDealerCards(dealerCard1);
   cardsView.gameStart();
@@ -75,14 +74,14 @@ cardsView.gameStart = function() {
   cardsView.render( playerCards );
   cardsView.render( dealerCards );
   // add back card image to dealer hand
-  $('.dealer-cards').append('<li><img src="images/card-back.png" alt="card-back"></li>');
+  $( '.dealer-cards' ).append( '<li><img src="images/card-back.png" alt="card-back"></li>' );
   cardsView.checkForBlackjack( playerCards );
 }
 
 cardsView.checkForBlackjack = function( cards ) {
   if( cardsView.sumOfcards( cards ) === 21 ){
     if( cards.length === 2 ) {
-        $('.chat').append('<h4>WINNER WINNER CHICKEN DINNER! You\'ve been dealt Blackjack!!! ツ</h4>');
+        $( '.chat' ).append( '<h4>WINNER WINNER CHICKEN DINNER! You\'ve been dealt Blackjack!!! ツ</h4>' );
     }
   }
 }
@@ -90,20 +89,20 @@ cardsView.checkForBlackjack = function( cards ) {
 cardsView.checkForBust = function( cards ) {
   if( cards == playerCards ) {
     if( cardsView.sumOfcards( playerCards ) > 21 ) {
-      $('#hit').attr('disabled', 'true');
-      $('#stand').attr('disabled', 'true');
-      $('#surrender').attr('disabled', 'true');
-      $('.chat').append('<h4>Player, you just busted. House wins.</h4>');
-      $('.chat').append('<h4>You are being dealt a new hand.</h4>');
+      $( '#hit' ).attr( 'disabled', 'true' );
+      $( '#stand' ).attr( 'disabled', 'true' );
+      $( '#surrender' ).attr( 'disabled', 'true' );
+      $( '.chat' ).append( '<h4>Player, you just busted. House wins.</h4>' );
+      $( '.chat' ).append( '<h4>You are being dealt a new hand.</h4>' );
       cardsView.newHand();
     }
   } else if( cards == dealerCards ) {
     if( cardsView.sumOfcards( dealerCards ) > 21 ) {
-      $('#hit').attr('disabled', 'true');
-      $('#stand').attr('disabled', 'true');
-      $('#surrender').attr('disabled', 'true');
-      $('.chat').append('<h4>Dealer just busted! You WIIIIINN!</h4>');
-      $('.chat').append('<h4>You are being dealt a new hand.</h4>');
+      $( '#hit' ).attr( 'disabled', 'true' );
+      $( '#stand' ).attr( 'disabled', 'true' );
+      $( '#surrender' ).attr( 'disabled', 'true' );
+      $( '.chat' ).append( '<h4>Dealer just busted! You WIIIIINN!</h4>' );
+      $( '.chat' ).append( '<h4>You are being dealt a new hand.</h4>' );
       cardsView.newHand();
     }
   }
@@ -121,19 +120,17 @@ cardsView.shoeSize = function() {
     DeckOfCards.all = [];
     DeckOfCards.fetchAll();
     cardsView.newGame();
-
   }
 }
 // //Alert i an A is dealt
 // cardsView.alertAce = function() {
 //     $('.chat').append('<h4>Congratulations, you\'ve been dealt an Ace!!! You can use your Ace as a "1" or "11."</h4>');
 //   }
-//
 
 cardsView.dealerTurn = function() {
   //Add up dealer cards
   //while dealer cards <17, hit, otherwise stand and log total to chat
-  while( cardsView.sumOfcards( dealerCards ) < 17) {
+  while( cardsView.sumOfcards( dealerCards ) < 17 ) {
     let newCard = cardsView.hit();
     dealerCards.push( newCard );
     console.log( dealerCards );
@@ -161,14 +158,14 @@ cardsView.compareHands = function() {
   playerHandValue = cardsView.sumOfcards( playerCards );
   dealerHandValue = cardsView.sumOfcards( dealerCards );
   if( playerHandValue === dealerHandValue ) {
-    $('.chat').append('<h4>It\'s a push. You and the dealer have same hand...</h4>');
+    $( '.chat' ).append( '<h4>It\'s a push. You and the dealer have same hand...</h4>' );
   } else if ( playerHandValue > dealerHandValue ) {
-    $('.chat').append('<h4>Dealer\'s hand value is only ' + dealerHandValue +'. Player\'s hand value is ' + playerHandValue + '. Player WINNNS!</h4>');
+    $( '.chat' ).append( '<h4>Dealer\'s hand value is only ' + dealerHandValue +'. Player\'s hand value is ' + playerHandValue + '. Player WINNNS!</h4>' );
   } else if ( dealerHandValue > playerHandValue ) {
-    $('.chat').append('<h4>Dealer\'s hand value is ' + dealerHandValue +'. Dealer wins. Better luck next time!</h4>');
+    $( '.chat' ).append( '<h4>Dealer\'s hand value is ' + dealerHandValue +'. Dealer wins. Better luck next time!</h4>' );
   }
   cardsView.newHand();
-  $('.chat').append('<h4>You are being dealt a new hand.</h4>');
+  $( '.chat' ).append( '<h4>You are being dealt a new hand.</h4>' );
 }
 
 //TODO: Who won message, instructions for how to start a new hand. Should the message be different depending on whether player or dealer won?
@@ -186,16 +183,15 @@ cardsView.surrenderMessage = function() {
 
 
   //Start New Game
-  $('#new-game').on('click', function() {
+  $( '#new-game' ).on( 'click', function() {
     cardsView.newGame();
-
   });
 
   //TODO: on click of hit me button, generate a new card/value and add it to the total score
-  $('#hit').on('click', function() {
-    console.log("Hit was pressed")
+  $( '#hit' ).on( 'click', function() {
+    //console.log( "Hit was pressed" )
     let newCard = cardsView.hit();
-    playerCards.push(newCard);
+    playerCards.push( newCard );
     cardsView.render( playerCards );
     cardsView.checkForBust( playerCards );
     //Added Card Face Values
@@ -206,20 +202,20 @@ cardsView.surrenderMessage = function() {
   });
 
   //TODO: on click of stand button, reveal(generate) dealer second card. Series of messages telling user where the dealer is at and what action dealer needs to take
-  $('#stand').on('click', function(){
-    $('.chat').append('<h4>You have chosen to stand on ' + cardsView.sumOfcards(playerCards) + '.</h4>');
-    $('#stand').attr('disabled', 'true');
-    $('#hit').attr('disabled', 'true');
-    $('#surrender').attr('disabled', 'true');
+  $( '#stand' ).on( 'click', function(){
+    $( '.chat' ).append( '<h4>You have chosen to stand on ' + cardsView.sumOfcards(playerCards) + '.</h4>' );
+    $( '#stand' ).attr( 'disabled', 'true' );
+    $( '#hit' ).attr( 'disabled', 'true' );
+    $( '#surrender' ).attr( 'disabled', 'true' );
     cardsView.dealerTurn();
   });
 
   //dealerCards.push(cardsView.hit());
   //console.log('dealerCards', dealerCards);
   //TODO: surrender method
-  $('#surrender').on('click', function(){
-    $('.chat').append('<h4>You have surrendered. Dealer wins!</h4>');
-    $('#surrender').attr('disabled', 'true');
+  $( '#surrender' ).on( 'click', function(){
+    $( '.chat' ).append( '<h4>You have surrendered. Dealer wins!</h4>' );
+    $( '#surrender' ).attr( 'disabled', 'true' );
     cardsView.newHand();
     // cardsView.newGame();
   });
